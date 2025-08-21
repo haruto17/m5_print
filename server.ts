89b2e89564162e8f7b55891dca6812925804a1ed
haruto17 <proaca157@gmail.com>
2025-08-22 05:01:24 +0900

    🎉 initial commit

---
+++ server.ts
@@ -0,0 +1,12 @@
+import { serveDir } from "@std/http/file-server";
+
+Deno.serve((req) => {
+  const pathname = new URL(req.url).pathname;
+  console.log(pathname);
+
+  return serveDir(req, {
+    fsRoot: "public",
+    showDirListing: true,
+    enableCors: true,
+  });
+});
